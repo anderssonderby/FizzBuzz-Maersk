3922e38786691e2941fd0cb5df3ae17a780516dd
anderssonderby <50653739+anderssonderby@users.noreply.github.com>
2022-04-04 18:17:14 +0200

    Add FizzBuzz algorithm

---
+++ FizzBuzz-Maersk/FizzBuzz-Maersk/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿namespace FizzBuzz_Maersk
+{
+    public class FizzBuzz
+    {
+        public string[] CalculateFizzBuzz(int[] input)
+        {
+            var result = new string[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                // If input modulo 3 and 5 (divisible by 3 and 5) == 0, print FizzBuzz
+                if (((input[i] % 3) == 0) & ((input[i] % 5) == 0))
+                {
+                    result[i] = "FizzBuzz";
+                }
+                // If input modulo 3 (divisible by 3) == 0, print Fizz
+                else if ((input[i] % 3) == 0)
+                {
+                    result[i] = "Fizz";
+                }
+                // If input modulo 3 (divisible by 5) == 0, print Buzz
+                else if ((input[i] % 5) == 0)
+                {
+                    result[i] = "Buzz";
+                }
+                // If not divisible by 3 or 5, print the number itself
+                else
+                {
+                    result[i] = input[i].ToString();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FizzBuzz-Maersk/FizzBuzz-Maersk/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzz_Maersk;
+
+public class Program
+{
+    static void Main()
+    {
+        FizzBuzz fizz = new();
+
+        var inputArray = new int[] { 1, 2, 3, 4, 5, 6, 15 };
+
+        var result = fizz.CalculateFizzBuzz(inputArray);
+
+        foreach (var r in result)
+        {
+            Console.WriteLine(r);   
+        }
+    }
+}
